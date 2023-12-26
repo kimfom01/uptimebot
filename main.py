1bbe4ddc823909893186a0544003172920181080
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-26 16:50:43 +0300

    Remove the hardcoded url from function

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import requests
 import time
 
 
-def make_request():
-    response = requests.get("https://quicknotes.space/")
+def make_request(url: str):
+    response = requests.get(url=url)
 
     print("Response status code: ", response.status_code)
 
 
 SLEEP_TIME = 600
+URL = "https://quicknotes.space/"
 
 while True:
-    make_request()
+    make_request(url=URL)
     time.sleep(SLEEP_TIME)
