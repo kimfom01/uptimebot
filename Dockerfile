7f7c8e0265848f19327e6afd658cbd47b78ffec5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-11-19 08:13:54 +0300

    ✨ Print respons message along with status code
    Improve docker build script

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM python:3.10-slim
 
 WORKDIR /bot
 
-ADD . .
+COPY requirements.txt .
 
 RUN pip install -r ./requirements.txt
+
+COPY . .
 
 CMD [ "python", "-u", "main.py" ]
--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import time
 def make_request(url: str):
     response = requests.get(url=url)
 
-    print("Response status code: ", response.status_code)
+    print(
+        "Response status code: ",
+        response.status_code,
+        "=> message: ",
+        response.text,
+    )
 
 
-SLEEP_TIME = 600
-URL = "https://quicknotes.space/"
+SLEEP_TIME = 180
+URL = "https://emailpostapi.onrender.com/health"
 
 while True:
     make_request(url=URL)
